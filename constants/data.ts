f31b42a50a80ff1054815155cf95ebbd27300af3
Aleksandar Cvetković <office@e-seo.info>
2026-01-15 04:10:30 +0100

    feat: add data source constants for traffic accident datasets (2020-2025)

---
+++ constants/data.ts
@@ -0,0 +1,15 @@
+// constants/dataSources.ts ili slično
+export const DATA_SOURCES = {
+  "2020":
+    "https://data.gov.rs/sr/datasets/r/05df84c8-925c-4b71-b77f-899faa73be48",
+  "2021":
+    "https://data.gov.rs/sr/datasets/r/14dc2a35-1d23-40ce-ab14-8872b7c552f4",
+  "2022":
+    "https://data.gov.rs/sr/datasets/r/725bc318-e00e-4db3-aa1f-ab5ff5100799",
+  "2023":
+    "https://data.gov.rs/sr/datasets/r/fe778c8a-d02b-4a3e-abbf-4c5acc724a44",
+  "2024":
+    "https://data.gov.rs/sr/datasets/r/9fc37c8a-d90f-4442-82fc-4203ab6f5f77",
+  "2025":
+    "https://data.gov.rs/sr/datasets/r/a86a3683-000e-481b-9111-c1326966c460",
+} as const;
